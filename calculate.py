6b1c5af6d27ece4010dcf35d6f10fe99bdb99aad
Tejas Ravishankar <xtremedevx@gmail.com>
2021-08-19 12:46:22 +0400

    feat: `calculate.py` for calculating percentages

--- calculate.py
+++ calculate.py
@@ -1,5 +1,6 @@
 import json
 import os
+from math import floor
 
 data = {}
 
@@ -7,9 +8,18 @@ for file in os.listdir('indexed'):
     with open('indexed/' + file, 'r') as f:
         data.update(json.load(f))
 
-count = 0
+total_count = 0
 
 for (package, value) in data.items():
-    count += value
+    total_count += value
 
-print(count)
+top_1_pc = floor(1 / 100 * len(data.items()))
+top_1_count = 0
+
+keys = list(data.keys())
+
+for key in keys[:top_1_pc]:
+    top_1_count += data[key]
+
+print(f'Top 50%: {top_1_count} vs Total: {total_count}')
+print(f'Top 50% Forms {(top_1_count / total_count) * 100}% of the {total_count} total downloads')
